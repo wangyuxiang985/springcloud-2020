cc9aab936ee78f298a66aa9715f74cf82cde932d
wangyuxiang985 <wangyuxiang985@gmail.com>
2020-03-19 11:57:40 +0800

    add 添加配置，使3355手动动态更新配置文件

--- cloud-config-client3355/src/main/java/com/yuxiang/springcloud/config/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/yuxiang/springcloud/config/ConfigClientController.java
@@ -1,10 +1,16 @@
 package com.yuxiang.springcloud.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 手动版自动刷新读取配置文件，需要运维人员手动发送一个post请求，让3355重新去配置中心读取
+ * http://localhost:3355/actuator/refresh
+ */
 @RestController
+@RefreshScope //用户自动刷新配置文件
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
