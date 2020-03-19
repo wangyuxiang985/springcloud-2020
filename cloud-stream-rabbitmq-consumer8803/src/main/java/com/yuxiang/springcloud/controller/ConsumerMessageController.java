ae0ccf1ab4a8a7ed1652db4568329aad639c3bd9
wangyuxiang985 <wangyuxiang985@gmail.com>
2020-03-19 20:12:27 +0800

    add 依照消息消费者8802构建8803实现多消费者

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/yuxiang/springcloud/controller/ConsumerMessageController.java
@@ -0,0 +1,22 @@
+package com.yuxiang.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)
+public class ConsumerMessageController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        String payload = message.getPayload();
+        System.out.println("消费者2号，接收到的消息--->" + payload + "\t,serverPort:" + serverPort);
+    }
+}
