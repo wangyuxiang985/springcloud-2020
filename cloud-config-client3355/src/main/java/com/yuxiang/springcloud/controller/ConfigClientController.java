7e1effcc67295660ba8634400aa7f7e34fb295ce
wangyuxiang985 <wangyuxiang985@gmail.com>
2020-03-19 15:06:41 +0800

    add 依照3355扩展一个配置客户端3366

--- cloud-config-client3355/src/main/java/com/yuxiang/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/yuxiang/springcloud/controller/ConfigClientController.java
@@ -1,4 +1,4 @@
-package com.yuxiang.springcloud.config;
+package com.yuxiang.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
